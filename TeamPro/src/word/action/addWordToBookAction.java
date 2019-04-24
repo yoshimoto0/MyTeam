6f38393d31b3ad563424e88ee7242916305acadc
gihoon <gihoon.gwon@gmail.com>
2019-04-24 14:36:57 +0900

    190424 ing2

---
+++ TeamPro/src/word/action/addWordToBookAction.java
@@ -0,0 +1,23 @@
+package word.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import word.vo.ActionForward;
+import word.vo.BookDTO;
+
+public class addWordToBookAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		HttpSession session = request.getSession();
+		int wordnum = Integer.parseInt("wordNum");
+		
+		BookDTO dto = new BookDTO((String)session.getAttribute("user_id"), wordnum, (String)session.getAttribute("book_id"),0);
+		
+		return ;
+	}
+
+}
