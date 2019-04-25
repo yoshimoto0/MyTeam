505f00677a841db0e1a5d75442bd88e74fb07f0f
gihoon <gihoon.gwon@gmail.com>
2019-04-25 16:05:29 +0900

    190425

--- TeamPro/table.sql
+++ TeamPro/table.sql
@@ -9,6 +9,7 @@ create table member(
 	isadmin number default 0 not null
 );
 
+insert into member values('aaa','1234','aaa','aaa.jpg','email@email.com',1,0);
 
 drop table word;
 create table word(
@@ -20,6 +21,30 @@ create table word(
 	
 );
 
+insert into word values(word_num.nextval, 'aa','aaaa',1);
+insert into word values(word_num.nextval, 'bb','bbbb',2);
+insert into word values(word_num.nextval, 'cc','cccc',3);
+insert into word values(word_num.nextval, 'dd','dddd',1);
+insert into word values(word_num.nextval, 'ee','eeee',2);
+insert into word values(word_num.nextval, 'ff','ffff',3);
+insert into word values(word_num.nextval, 'gg','gggg',1);
+insert into word values(word_num.nextval, 'hh','hhhh',2);
+insert into word values(word_num.nextval, 'ii','iiii',3);
+insert into word values(word_num.nextval, 'jj','jjjj',1);
+insert into word values(word_num.nextval, 'kk','kkkk',2);
+insert into word values(word_num.nextval, 'll','llll',3);
+insert into word values(word_num.nextval, 'mm','mmmm',1);
+insert into word values(word_num.nextval, 'nn','nnnn',2);
+insert into word values(word_num.nextval, 'oo','oooo',3);
+insert into word values(word_num.nextval, 'pp','pppp',1);
+insert into word values(word_num.nextval, 'qq','qqqq',2);
+insert into word values(word_num.nextval, 'rr','rrrr',3);
+insert into word values(word_num.nextval, 'ss','ssss',1);
+insert into word values(word_num.nextval, 'tt','tttt',2);
+insert into word values(word_num.nextval, 'uu','uuuu',3);
+insert into word values(word_num.nextval, 'vv','vvvv',1);
+insert into word values(word_num.nextval, 'ww','aaaa',2);
+insert into word values(word_num.nextval, 'xx','aaaa',3);
 
 drop sequence word_num;
 create sequence word_num;
@@ -38,6 +63,14 @@ create table book_list(
 drop sequence book_id;
 create sequence book_id;
 
+insert into book_list values(book_id.nextval,'b-aaa', 'aaa', 1,0);
+insert into book_list values(book_id.nextval,'b-bbb', 'aaa', 2,0);
+insert into book_list values(book_id.nextval,'b-ccc', 'aaa', 3,0);
+insert into book_list values(book_id.nextval,'b-ddd', 'aaa', 1,0);
+insert into book_list values(book_id.nextval,'b-eee', 'aaa', 2,0);
+insert into book_list values(book_id.nextval,'b-fff', 'aaa', 3,0);
+
+
 drop table book;
 create table book(    
 	user_id varchar2(20) not null,
@@ -50,7 +83,35 @@ create table book(
 	CONSTRAINT fk_user_id FOREIGN KEY(user_id) REFERENCES member(user_id) ON DELETE CASCADE
 );
 
+insert into book values('aaa', 1, 1, 0);
+insert into book values('aaa', 2, 2, 0);
+insert into book values('aaa', 3, 3, 0);
+insert into book values('aaa', 4, 4, 0);
+insert into book values('aaa', 5, 5, 0);
+insert into book values('aaa', 6, 6, 0);
+insert into book values('aaa', 7, 1, 0);
+insert into book values('aaa', 8, 2, 0);
+insert into book values('aaa', 9, 3, 0);
+insert into book values('aaa', 10, 4, 0);
+insert into book values('aaa', 11, 5, 0);
+insert into book values('aaa', 12, 6, 0);
+insert into book values('aaa', 13, 1, 0);
+insert into book values('aaa', 14, 2, 0);
+insert into book values('aaa', 15, 3, 0);
+insert into book values('aaa', 16, 4, 0);
+insert into book values('aaa', 17, 5, 0);
+insert into book values('aaa', 18, 6, 0);
+insert into book values('aaa', 19, 1, 0);
+insert into book values('aaa', 20, 2, 0);
+insert into book values('aaa', 21, 3, 0);
+insert into book values('aaa', 22, 4, 0);
+insert into book values('aaa', 23, 5, 0);
+insert into book values('aaa', 24, 6, 0);
 
+select * from book;
+delete from book_list where book_id = 1;
+select book.wordNum, word, meaning, star 
+from book, word where user_id = 'aaa' and book_id = 3 and book.wordnum = word.num;
 drop table kind;
 create table kind(
 	kind_id number primary key,
